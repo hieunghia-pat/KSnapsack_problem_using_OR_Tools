8d6255cda6068505cb8b7599dee3fc617fd7ca92
hieunghia-pat <19520178@gm.uit.edu.vn>
2021-10-22 14:27:07 +0700

    create repository

---
+++ README.md
@@ -0,0 +1,52 @@
+Knapsack Problem solver using (OR Tools)[https://developers.google.com/optimization] from Google Developer.
+
+===
+In this repo, I used OR Tools to get solutions for various test cases of Knapsack Problem. These test cases come from (kplib)[https://github.com/likr/kplib].
+
+I examined about 5 marks of limit time (1s, 3s, 60s, 120s and 180s) and observe the effect of time limitation for the ability to find the optimal solutions of Knapsack solver from OR Tools. But because of the limitation of hardware, some test cases only have about 2-3 results with corresponding time limitation.
+
+To reproduce my results, change directory to solvers/build then rebuild or run main target file with appropriate **absolute** directory to the test cases folders. For example: ./main $HOME/Projects/kplib/02StronglyCorrelated
+
+### Dependencies:
+1. OR Tools.
+2. CMake.
+3. g++ 9.x with standard 17.
+4. (Boost)[https://www.boost.org/] library.
+
+# kplib
+
+Test instances for 0-1 knapsack problems
+
+The instances were generated according to the following book chapter. 
+
+> Kellerer, H., Pferschy, U., & Pisinger, D. (2004). Exact solution of the knapsack problem. In Knapsack Problems (pp. 117-160). Springer, Berlin, Heidelberg.
+
+The generation code is available here.
+
+* https://bitbucket.org/likr/mnkproblems/
+* https://bitbucket.org/likr/mnkptools/
+
+(However, updating this code has stopped...)
+
+## Data format
+
+The following equation is a formulation of 0-1 knapsack problems.
+
+<img src="https://latex.codecogs.com/gif.latex?%5Cbegin%7Baligned%7D%20%5Cmax%20%26%20%5Csum_%7Bi%3D1%7D%5En%20p_i%20x_i%20%5C%5C%20%5Ctext%7Bs.t.%7D%20%26%20%5Csum_%7Bi%3D1%7D%5En%20w_i%20x_i%20%5Cleq%20c%2C%20%5C%5C%20%26%20x_i%20%5Cin%20%5C%7B0%2C%201%5C%7D.%20%5Cend%7Baligned%7D" />
+
+A problem instance are described in the following data format in `.kp` file.
+
+```
+n
+c
+
+p_1 w_1
+p_2 w_2
+…
+p_n w_n
+```
+
+
+## License
+
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
---
+++ solvers/main.cpp
@@ -0,0 +1,165 @@
+#include <fstream>
+#include <iostream>
+#include "ortools/algorithms/knapsack_solver.h"
+#include <vector>
+#include <tuple>
+#include <filesystem>
+#include <string>
+#include <sstream>
+#include <ctime>
+#include <experimental/random>
+#include <memory>
+#include <boost/format.hpp>
+
+namespace fs = std::filesystem;
+namespace opr = operations_research;
+
+std::tuple<std::vector<std::int64_t>, std::vector<std::int64_t>, std::int64_t, bool> get_solution(
+                                                    std::vector<std::vector<std::int64_t>> const& weights, 
+                                                    std::vector<std::int64_t> const& values, 
+                                                    std::vector<std::int64_t> const& capacities, std::int8_t limit_time)
+{
+    std::unique_ptr<opr::KnapsackSolver> solver = std::make_unique<opr::KnapsackSolver>(
+        opr::KnapsackSolver::KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER, 
+        "KNapSackSolver"
+    );
+
+    solver->set_time_limit(limit_time);
+    solver->Init(values, weights, capacities);
+
+    std::time_t startTime = std::time(nullptr);
+    std::int64_t total_value = solver->Solve();
+    std::time_t endTime = std::time(nullptr);
+    std::cout << "Time consuming: " << endTime - startTime << "s." << std::endl;
+    
+    std::vector<std::int64_t> packed_items;
+    for (std::size_t i = 0; i < values.size(); i++)
+    {
+        if (solver->BestSolutionContains(i))
+        {
+            packed_items.push_back(i);
+        }
+    }
+
+    std::vector<std::int64_t> packed_weights;
+    for (auto const& item: packed_items)
+    {
+        packed_weights.push_back(weights[0][item]);
+    }
+
+    return std::make_tuple<std::vector<std::int64_t>, std::vector<std::int64_t>, std::int64_t, bool>
+                            (std::move(packed_items), std::move(packed_weights), std::move(total_value), std::move(solver->IsSolutionOptimal()));
+
+}
+
+std::vector<std::string> split(std::string const& string, char delim)
+{
+    std::istringstream sstream {string};
+    std::vector<std::string> tokens;
+    std::string tmp_string;
+    while (std::getline(sstream, tmp_string, delim))
+    {
+        tokens.push_back(std::move(tmp_string));
+    }
+
+    return std::move(tokens);
+}
+
+int main(int argc, char* argv[])
+{
+    std::string path { argv[1] };
+
+    std::vector<std::string> ncases {"n00050", "n00100", /*"n00200",*/ "n00500", "n01000", "n00050", "n02000", "n05000", "n10000"};
+    std::vector<std::string> subcases {"R01000", "R10000"};
+    // std::vector<int> limit_times { 1, 3, 60, 120, 180};
+    std::vector<int> limit_times { 1, 3, 60};
+
+    for (auto const& ncase : ncases)
+    {
+        for (auto const& subcase: subcases)
+        {
+            fs::path parent_path(fs::path(path) / fs::path(ncase) / fs::path(subcase));
+            fs::directory_iterator files(parent_path);
+            
+            for (auto& limit_time: limit_times)
+            {
+                fs::path file_name = fs::begin(files)->path();
+                
+                std::string path = file_name.string();
+
+                // split the path to get the tree of directory
+                std::vector<std::string> tokens = split(path, '/');
+
+                // create directory for containing solutions
+                fs::path out_path { fs::current_path() / fs::path("..") };
+                for (auto token = tokens.begin()+5; token != tokens.end()-1; token++)
+                {
+                    out_path /= fs::path(token->data());
+                }
+                fs::create_directories(fs::path(out_path.string()));
+                std::string filename = split(tokens.back(), '.')[0];
+                std::cout << out_path << std::endl;
+                out_path /= (boost::format{"%1%_%2%s.txt"} % filename % limit_time).str();
+
+                std::ifstream in_file(path, std::ios_base::in);
+
+                std::vector<std::vector<std::int64_t>> weights(1);
+                std::vector<std::int64_t> values;
+
+                std::int16_t n;
+                std::vector<std::int64_t> capacities(1);
+
+                std::cout << "Loading data ..." << std::endl;
+                
+                in_file >> n >>  capacities[0];
+                for (std::int16_t id = 0; id < n; id++)
+                {
+                    std::int16_t value, weight;
+                    in_file >> value >> weight;
+                    values.push_back(value);
+                    weights[0].push_back(weight);
+                }
+
+                std::vector<std::int64_t> packed_items;
+                std::vector<std::int64_t> packed_weights;
+                std::int64_t total_value;
+                bool isOptimal;
+
+                std::cout << "Done." << std::endl;
+
+                std::cout << "Get solution for " << path << " ..." << std::endl;
+
+                std::tie(packed_items, packed_weights, total_value, isOptimal) = get_solution(weights, values, capacities, limit_time);
+
+                std::int64_t total_weight = std::accumulate(packed_weights.begin(), packed_weights.end(), std::int64_t(0));
+
+                std::cout << "Saving to " << out_path.string() << " ..." << std::endl;
+
+                std::ofstream out_file(out_path.string(), std::ios_base::out);
+
+                out_file << n << std::endl;
+                out_file << capacities[0] << std::endl;
+
+                std::ostringstream osstream;
+                std::move(packed_items.begin(), packed_items.end()-1, std::ostream_iterator<int>(osstream, ", "));
+
+                osstream << std::move(packed_items.back());
+                out_file << osstream.str() << std::endl;
+
+                std::move(packed_weights.begin(), packed_weights.end()-1, std::ostream_iterator<int>(osstream, ", "));
+                osstream << std::move(packed_weights.back());
+                out_file << osstream.str() << std::endl;
+
+                out_file << total_value << std::endl;
+                out_file << total_weight << std::endl;
+                out_file << isOptimal << std::endl;
+                
+                out_file.close();
+                std::cout << std::string(20, '+') << std::endl;
+            }
+            
+        }
+    }
+
+    return 0;
+}
